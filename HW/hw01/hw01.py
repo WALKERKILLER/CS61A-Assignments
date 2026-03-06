f91254f25e79c44eece3926eb656ec1c71de60b4
Sile Liu <walker_killer@163.com>
2026-03-06 08:47:51 +0800

    完成hw01

--- HW/hw01/hw01.py
+++ HW/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return pow(min(i,j,k),2) + pow(i+j+k-min(i,j,k)-max(i,j,k),2)
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -65,8 +65,10 @@ def largest_factor(n):
     >>> largest_factor(13) # factor is 1 since 13 is prime
     1
     """
-    "*** YOUR CODE HERE ***"
-
+    i = n - 1
+    while n % i != 0:
+        i -= 1
+    return i   
 
 def hailstone(n):
     """Print the hailstone sequence starting at n and return its
@@ -87,5 +89,13 @@ def hailstone(n):
     >>> b
     1
     """
-    "*** YOUR CODE HERE ***"
-
+    i = 1
+    print(n)
+    while n != 1:
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = 3 * n + 1
+        i += 1
+        print(int(n))
+    return i
